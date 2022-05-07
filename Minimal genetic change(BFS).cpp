685822b9228db5d2a6e12fcf7737d3f32a440aad
Pencil <446187673@qq.com>
2022-05-07 21:26:17 +0800

    DAY1

---
+++ Minimal genetic change(BFS).cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+//题目链接//https://leetcode-cn.com/problems/minimum-genetic-mutation/
+//题解 https://leetcode-cn.com/problems/minimum-genetic-mutation/solution/by-fuxuemingzhu-t1mv/
+//BFS 广度优先搜索
+//每一轮改变字母，变换成与bank中匹配的序列，就相当于生成了一层子节点
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        queue<string> que;
+        que.push(start);
+        const int n = start.size();
+        int step = 0;
+        while(!que.empty())
+        {
+            int size = que.size();
+            for (int i = 0; i < size; i++)
+            {
+                string cur = que.front();
+                que.pop();
+                if(cur == end)
+                    return step;  //如果遍历到对应的基因，就返回
+                for (int i = 0; i < n; i++)
+                {
+                    for(char changechar : {'A','C','G','T'})
+                    {
+                        string nxt = cur.substr(0,i) + changechar + cur.substr(i+1); //依次变动单个基因
+                        for(vector<string>::iterator it = bank.begin();it < bank.end(); it++)
+                        {
+                            if(*it == nxt)
+                            {
+                                que.push(nxt);
+                                bank.erase(it); //注意erase的参数为迭代器
+                            }
+                        }
+                    }
+                }
+            }
+            step++;  //全部找完，step加1，层数加1
+        }
+        return -1;
+
+    }
+};
+
+
+int main()
+{
+    Solution s1;
+    string start = "AACCGGTT";
+    string end = "AACCGGTA";
+    vector<string> bank{"AACCGGTA"};
+    cout<<s1.minMutation(start,end,bank);
+
+    system("Pause");
+}
--- test.cpp
+++ test.cpp
@@ -1,13 +1,51 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<string>
 #include<algorithm>
 
 using namespace std;
 
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        queue<string> q;
+        const int N = start.size();
+        q.push(start);
+        int step = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int s = 0; s < size; s++) {
+                auto cur = q.front(); q.pop();
+                if (cur == end) {
+                    return step;
+                }
+                for (int i = 0; i < N; i++) {
+                    for (char n : {'A', 'C', 'G', 'T'}) {
+                        string next = cur.substr(0, i) + n + cur.substr(i + 1);
+                        if (next == cur) continue;
+                        for (auto it = bank.begin(); it < bank.end(); ++it) {
+                            if (*it == next) {
+                                q.push(next);
+                                bank.erase(it);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            step += 1;
+        }
+        return -1;
+    }
+};
 int main()
 {
-    cout<<"安保部abb"<<endl;
-    cout<<1<<endl;
+    Solution s1;
+    string start = "AACCGGTT";
+    string end = "AACCGGTA";
+    vector<string> bank{"AACCGGTA"};
+    cout<<s1.minMutation(start,end,bank);
+
     system("Pause");
 }
