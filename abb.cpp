94138b8ba612bad550fcf207749dc1566ba1781a
Pencil <446187673@qq.com>
2022-05-07 18:10:45 +0800

    同步

---
+++ abb.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    cout<<"安保部abb"<<endl;
+    system("Pause");
+}
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    cout<<"安保部abb"<<endl;
+    cout<<1<<endl;
+    system("Pause");
+}
